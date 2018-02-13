fa111e8d9e955132f25eea474bc1c734178b828f
Vanessa Formicola <vanessa@Vanessas-MacBook-Pro.local>
2018-02-13 17:02:08 +0000

    ABD-41 Added missed file
    Authors: @michaelwalker @vanessaformicola

--- test/test_test.py
+++ test/test_test.py
@@ -4,5 +4,5 @@ from src import service_handler
 class TestStringMethods(unittest.TestCase):
 
     def test(self):
-        service_handler.hello_world()
+        service_handler.hello_world(None,None)
         self.assertEquals(1, 1)
