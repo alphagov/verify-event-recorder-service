b7a316c4c67e415c2909fcb631a9ef7769cccc7a
Vanessa Formicola <vanessa@Vanessas-MacBook-Pro.local>
2018-02-13 11:54:33 +0000

    ABD-41 Added setup script to define version of python
    Authors: @michaelwalker @vanessaformicola

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 pip3 install -r requirements.txt
-python3.6  -m unittest discover test/ "*_test.py"
+python3  -m unittest discover test/ "*_test.py"
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 pip3 install -r requirements.txt
-python3.6 src/service_handler.py
+python3 src/service_handler.py
 
