e0b47132e09f7469c43f1bce7c592009dce10747
Michael Walker <michael.walker@softwire.com>
2018-02-16 16:17:30 +0000

    ABD-41: Don't automatically install or update python.
    Make the startup script less aggressive, so that it does not automatically
    install or update python on your machine.
    
    Ideally, we would also verify the python version, but this requires too much
    string parsing to be worth the time at this point.
    
    Solo: @michaelwalker

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
-
+set -eu
 
 if [ -z "$(which python3)" ];
 then
-    echo "install"
-    brew install python3
-else
-    echo "update"
-    brew upgrade python3
+    echo -e "\033[31mError: Expected the alias 'python3' to exist\033[0m"
+    echo -e "To install python3, run"
+    echo -e "    brew install python3"
+    exit 1
 fi
 
 pip3 install virtualenv
