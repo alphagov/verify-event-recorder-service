0e1d81ebfee933e9da9bf18cb88f008eedf612b1
Michael Walker <michael.walker@softwire.com>
2018-02-20 12:16:03 +0000

    ABD-41: Now writes to a database.
    Solo: @michaelwalker

--- src/event_handler.py
+++ src/event_handler.py
@@ -1,5 +1,8 @@
 import boto3
 import os
+import json
+
+from src.db_helper import RunInTransaction, create_db_connection
 from src.event_mapper import event_from_json
 
 
@@ -7,6 +10,7 @@ from src.event_mapper import event_from_json
 def store_queued_events(_, __):
     sqs_client = boto3.client('sqs')
     queue_url = os.environ['QUEUE_URL']
+    db_connection = create_db_connection()
 
     while True:
         message = __fetch_single_message(sqs_client, queue_url)
@@ -17,10 +21,10 @@ def store_queued_events(_, __):
         # catch all errors and log them - we never want a single failing message to kill the process.
         try:
             event = event_from_json(message['Body'])
-            print(event.event_id)
+            __write_to_database(event, db_connection)
             __delete_message(sqs_client, queue_url, message)
         except Exception as exception:
-            print('Failed to store message - Exception: {0}'.format(str(message)))
+            print('Failed to store message - Exception: {0}'.format(str(exception)))
 
 
 def __fetch_single_message(sqs_client, queue_url):
@@ -31,6 +35,21 @@ def __fetch_single_message(sqs_client, queue_url):
         WaitTimeSeconds=0,  # Don't wait for messages - if there aren't any left, then this lambda's job is done
     )
     return response['Messages'][0] if 'Messages' in response and response['Messages'] else None
+
+
+def __write_to_database(event, db_connection):
+    with RunInTransaction(db_connection) as cursor:
+        cursor.execute("""
+            INSERT INTO events
+            (event_id, event_type, timestamp, details)
+            VALUES
+            ('%s', '%s', '%s', '%s');
+        """ % (
+            event.event_id,
+            event.event_type,
+            event.timestamp,
+            json.dumps(event.details)
+        ))
 
 
 def __delete_message(sqs_client, queue_url, message):
--- test/event_handler_test.py
+++ test/event_handler_test.py
@@ -3,24 +3,57 @@ import boto3
 import io
 import json
 import uuid
+import psycopg2
 from moto import mock_sqs
 from unittest import TestCase
 from unittest.mock import patch
+from datetime import datetime
 
 from src import event_handler
+from src.db_helper import RunInTransaction
 
+EVENT_TYPE = 'session_event'
+TIMESTAMP = '2018-02-10 12:07:32'
+SESSION_EVENT_TYPE = 'success'
 
 @mock_sqs
 class EventHandlerTest(TestCase):
+    __sqs_client = None
+    __queue_url = None
+    db_connection = None
+    db_connection_string = "host='localhost' dbname='postgres' user='postgres' password='secretPassword'"
+
+    @classmethod
+    def setUpClass(cls):
+        cls.db_connection = psycopg2.connect(cls.db_connection_string)
+        with RunInTransaction(cls.db_connection) as cursor:
+            cursor.execute("""
+                CREATE TABLE events (
+                    event_id VARCHAR(40) NOT NULL PRIMARY KEY,
+                    event_type VARCHAR(20) NOT NULL,
+                    timestamp TIMESTAMP NOT NULL,
+                    details JSON NOT NULL
+                );
+            """)
+
+    @classmethod
+    def tearDownClass(cls):
+        with RunInTransaction(cls.db_connection) as cursor:
+            cursor.execute("""
+                DROP TABLE events;
+            """)
 
     def setUp(self):
         setup_stub_aws_config()
         self.__sqs_client = boto3.client('sqs')
         self.__queue_url = create_queue(self.__sqs_client)
         add_queue_url_to_environment_variables(self.__queue_url)
+        add_connection_string_to_environment_variables(self.db_connection_string)
 
-    @patch('sys.stdout', new_callable=io.StringIO)
-    def test_reads_messages_from_queue(self, mock_stdout):
+    def tearDown(self):
+        self.__clean_db()
+
+    def test_reads_messages_from_queue(self):
         self.__send_messages_to_queue(
             [
                 create_event_string('sample-id-1'),
@@ -30,7 +63,7 @@ class EventHandlerTest(TestCase):
 
         event_handler.store_queued_events(None, None)
 
-        self.assertEqual(mock_stdout.getvalue(), 'sample-id-1\nsample-id-2\n')
+        self.__assert_database_has_records(['sample-id-1', 'sample-id-2'])
         self.assertEqual(self.__number_of_visible_messages(), '0')
         self.assertEqual(self.__number_of_hidden_messages(), '0')
 
@@ -45,8 +78,8 @@ class EventHandlerTest(TestCase):
 
         event_handler.store_queued_events(None, None)
 
+        self.__assert_database_has_records(['sample-id-2'])
         self.assertRegex(mock_stdout.getvalue(), 'Failed to store message - Exception:')
-        self.assertRegex(mock_stdout.getvalue(), 'sample-id-2')
         self.assertEqual(self.__number_of_visible_messages(), '0')
         self.assertEqual(self.__number_of_hidden_messages(), '1')
 
@@ -71,6 +104,25 @@ class EventHandlerTest(TestCase):
         )
         return response['Attributes'][attribute]
 
+    def __clean_db(self):
+        with RunInTransaction(self.db_connection) as cursor:
+            cursor.execute("""
+                DELETE FROM events;
+            """)
+
+    def __assert_database_has_records(self, expected_event_ids):
+        for event_id in expected_event_ids:
+            with RunInTransaction(self.db_connection) as cursor:
+                cursor.execute("""
+                    SELECT * FROM events WHERE event_id = '%s';
+                """ % event_id)
+                matching_records = cursor.fetchone()
+
+            self.assertEqual(matching_records[0], event_id)
+            self.assertEqual(matching_records[1], EVENT_TYPE)
+            self.assertEqual(matching_records[2], datetime.strptime(TIMESTAMP, '%Y-%m-%d %H:%M:%S'))
+            self.assertEqual(matching_records[3], {'sessionEventType': SESSION_EVENT_TYPE})
+
 
 def setup_stub_aws_config():
     os.environ = {
@@ -89,12 +141,16 @@ def add_queue_url_to_environment_variables(queue_url):
     os.environ['QUEUE_URL'] = queue_url
 
 
+def add_connection_string_to_environment_variables(connection_string):
+    os.environ['DB_CONNECTION_STRING'] = connection_string
+
+
 def create_event_string(event_id):
     return json.dumps({
         'eventId': event_id,
-        'eventType': 'session_event',
-        'timestamp': '2018-02-10:12:00:00',
+        'eventType': EVENT_TYPE,
+        'timestamp': TIMESTAMP,
         'details': {
-            'sessionEventType': 'success'
+            'sessionEventType': SESSION_EVENT_TYPE
         }
     })
