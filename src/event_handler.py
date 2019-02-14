e41a819288ab05bbb519cb85cec913b95466f453
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2019-02-14 13:43:22 +0000

    BAU: Fix Logging Errors from PR #34
    PR #34 introduced additional logging, however, it contain an error (https://github.com/alphagov/verify-event-recorder-service/pull/34)
    Corrected order of execution so that `event` is evaluated before being used in an log statement.

--- src/event_handler.py
+++ src/event_handler.py
@@ -18,33 +18,47 @@ def store_queued_events(_, __):
     sqs_client = boto3.client('sqs')
     queue_url = os.environ['QUEUE_URL']
 
+    logger = logging.getLogger('event-recorder')
     if 'ENCRYPTION_KEY' in os.environ:
       encrypted_decryption_key = os.environ['ENCRYPTION_KEY']
+      logger.info('Got decryption key from environment variable')
     else:
       encrypted_decryption_key = fetch_decryption_key()
+      logger.info('Got decryption key from S3')
     decryption_key = decrypt(encrypted_decryption_key)
+    logger.info('Decrypted key successfully')
 
     database_password = None
     if 'ENCRYPTED_DATABASE_PASSWORD' in os.environ:
       # boto returns decrypted as b'bytes' so decode to convert to password string
       database_password = decrypt(os.environ['ENCRYPTED_DATABASE_PASSWORD']).decode()
     db_connection = create_db_connection(database_password)
+    logger.info('Created connection to DB')
 
+    event_count = 0
     while True:
         message = fetch_single_message(sqs_client, queue_url)
         if message is None:
+            logger.info('Queue is empty - finishing after {0} events'.format(event_count))
             break
+
+        event_count += 1
 
         # noinspection PyBroadException
         # catch all errors and log them - we never want a single failing message to kill the process.
         try:
             decrypted_message = decrypt_message(message['Body'], decryption_key)
             event = event_from_json(decrypted_message)
+            logger.info('Decrypted event with ID: {0}'.format(event.event_id))
             write_audit_event_to_database(event, db_connection)
+            logger.info('Stored audit event: {0}'.format(event.event_id))
             if event.event_type == 'session_event' and event.details.get('session_event_type') == 'idp_authn_succeeded':
                 write_billing_event_to_database(event, db_connection)
+                logger.info('Stored billing event: {0}'.format(event.event_id))
             if event.event_type == 'session_event' and event.details.get('session_event_type') == 'fraud_detected':
                 write_fraud_event_to_database(event, db_connection)
+                logger.info('Stored fraud event: {0}'.format(event.event_id))
             delete_message(sqs_client, queue_url, message)
+            logger.info('Deleted event from queue with ID: {0}'.format(event.event_id))
         except Exception as exception:
-            logging.getLogger('event-recorder').exception('Failed to store message')
+            logger.exception('Failed to store message')
