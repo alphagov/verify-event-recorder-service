59ce75004c83330fd5c6a6f57a1a27520895bf59
brenetic <b@brenetic.com>
2019-04-30 18:32:15 +0100

    Remove database scripts

--- database-schema/permissions/revoke_writer_permissions.sql
+++
@@ -1,9 +0,0 @@
-REVOKE ALL PRIVILEGES ON DATABASE events FROM writer;
-REVOKE CONNECT ON DATABASE events FROM writer;
-REVOKE USAGE ON SCHEMA audit FROM writer;
-REVOKE USAGE ON SCHEMA billing FROM writer;
-REVOKE SELECT, INSERT, DELETE ON audit.audit_events FROM writer;
-REVOKE SELECT, INSERT, DELETE ON billing.billing_events FROM writer;
-REVOKE SELECT, INSERT, DELETE ON billing.fraud_events FROM writer;
--- Production and staging only
-REVOKE SELECT, INSERT, DELETE ON billing.billing_idps FROM writer;
